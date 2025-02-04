d69131d29e528ff00e4fb5d5cf25c378c5c715b5
Logan Winscher <logan.winscher@g.austincc.edu>
2025-02-03 23:33:45 -0600

    did outgoing string function

--- Game1377_Exercises/Excercise3.cs
+++ Game1377_Exercises/Excercise3.cs
@@ -12,7 +12,14 @@
         }
         static void OutgoingStrings()
         {
-            Console.WriteLine("");
+            string Game1377 = "I'm in Game 1377: Scripting for Game Designers.";
+            Console.WriteLine(Game1377);
+            Console.WriteLine(Game1377 + "\n");
+            Console.WriteLine(Game1377.ToUpper());
+            Console.WriteLine(Game1377.ToLower());
+            Console.WriteLine(Game1377.Substring(7));
+            Console.WriteLine(Game1377.Substring(7, 4));
+            Console.WriteLine(Game1377.Substring(7, 1));
         }
         static void Arithmetic()
         {
